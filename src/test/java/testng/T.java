119e044f5980609b08ef9c97d8c25b57d667dbc6
Ankit <Ankit@DESKTOP-0L876N7>
2021-06-06 16:27:18 +0530

    Practise1

---
+++ src/test/java/testng/T.java
@@ -0,0 +1,13 @@
+package testng;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class T {
+	public class test1 {
+		@Test
+		public void demo() {
+			Reporter.log("hello selenium",true);
+		}
+		}
+}
